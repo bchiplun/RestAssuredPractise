4ff5c410921c7cbd54437dfe346dfc57606bd75c
Bhavesh Chiplulnkar <bchiplunkar7@gmail.com>
2022-03-22 11:51:05 +0530

    Getting started with RestAssured

---
+++ src/PayLoad.java
@@ -0,0 +1,26 @@
+
+public class PayLoad {
+	
+	public static String addPlace()
+	{
+		return "{\r\n"
+				+ "  \"location\": {\r\n"
+				+ "    \"lat\": -38.383494,\r\n"
+				+ "    \"lng\": 33.427362\r\n"
+				+ "  },\r\n"
+				+ "  \"accuracy\": 50,\r\n"
+				+ "  \"name\": \"Bhaveshs organization\",\r\n"
+				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+				+ "  \"address\": \"29, side layout, cohen q9\",\r\n"
+				+ "  \"types\": [\r\n"
+				+ "    \"shoe park\",\r\n"
+				+ "    \"shop\"\r\n"
+				+ "      ],\r\n"
+				+ "  \"website\": \"http://google.com\",\r\n"
+				+ "  \"language\": \"French-IN\"\r\n"
+				+ "}\r\n"
+				+ "\r\n"
+				+ "";
+	}
+
+}
---
+++ src/Section04_Basics.java
@@ -0,0 +1,35 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class Section04_Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Restassured.BaseURI --> to give baseURI
+		//given --> to provide query parameters, headers, Payload --> all input details
+		//when --> to mention requet type and provide resources --> Submit API
+		//then --> to get and assert response
+		
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		
+		String res = given().log().all().header("content-type","application/json").queryParam("key", "qaclick123")
+		.body(PayLoad.addPlace())
+		
+		.when().post("maps/api/place/add/json")
+		
+		.then().assertThat().statusCode(200).header("Server", "Apache/2.4.18 (Ubuntu)").body("scope", equalTo("APP"))
+		.extract().response().asString();
+
+		//extracting data from response body
+		System.out.println(res);
+		
+		JsonPath js = new JsonPath(res);
+		
+		System.out.println(js.getString("place_id"));
+	}
+
+}
